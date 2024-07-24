f489bcd2d6ae6aae89b2bd5c47b3a83d3952473d
sapir <ohavasapir@gmail.com>
2024-07-24 20:49:52 +0300

    adding crud transactions

--- frontend/src/pages/TransactionPage.jsx
+++ frontend/src/pages/TransactionPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import TransactionFormSkeleton from "../components/skeletons/TransactionFormSkeleton";
+//import TransactionFormSkeleton from "../components/skeletons/TransactionFormSkeleton";
 
 const TransactionPage = () => {
   const [formData, setFormData] = useState({
